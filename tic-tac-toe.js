addfd0cc300ca930c6ec346b5aa5fcaaf1eec99a
KOUGD7 <cougahdere@yahoo.com>
2020-10-09 04:51:09 -0500

    Add X or O to selected square

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,32 @@
 window.addEventListener("load", addSquares);
 
+//Exercise 1 - Layout the board 
 function addSquares() {
     let boxes = document.querySelectorAll('#board div')
 
     boxes.forEach(element => {
         element.className = "square";
+        addXO(element)
     });
+    //console.log(boxes);
+}
 
-    console.log(boxes);
+//Exercise 2 - Add an X or O to a square when clicked
+function addXO(ele){
+    ele.addEventListener("click", function() { 
+        let xo = ele.textContent
+        if (xo == "O" ){
+            ele.textContent = "X";
+            ele.classList.remove('O');
+            ele.classList.add('X');
+        }
+        else{
+            ele.textContent = "O";
+            ele.classList.remove('X');
+            ele.classList.add('O');
+        }
+        //console.log(ele.textContent)
+        //console.log("Button clicked.");
+        }); 
+    
 }
